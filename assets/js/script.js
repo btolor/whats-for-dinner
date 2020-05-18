d8bcfb0b086ff9fc2730b83b8dbcf11ee4700ffc
btolor <brandontolor@yahoo.com>
2020-05-18 00:56:05 -0500

    Fixed the changing backgrounds...

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,29 +50,55 @@ $(() => {
     //         if (xhr.status == 404) { }
     //     }
     // })
-    let images = [
-        '/assets/images/slides/chocolates-and-raspberries-918327.jpg',
+    let landingImages = [
         'assets/images/slides/close-up-cold-color-drop-372882.jpg',
         'assets/images/slides/cook-cooking-cuisine-delicious-357737.jpg',
         'assets/images/slides/five-white-plates-with-different-kinds-of-dishes-54455.jpg',
         '/assets/images/slides/food-on-table-326278.jpg',
-        'assets/images/slides/shallow-focus-photography-of-chocolate-cupcakes-1028714.jpg'
+        'assets/images/slides/shallow-focus-photography-of-chocolate-cupcakes-1028714.jpg',
+        '/assets/images/slides/chocolates-and-raspberries-918327.jpg'
     ]
-    let counter = 0
+
+    let contentImages = [
+        'assets/images/content-slides/egg-avocado-cooking-tomato-106877.jpg',
+        'assets/images/content-slides/food-on-white-background-256318.jpg',
+        'assets/images/content-slides/fruits-eating-food-on-wood-326268.jpg',
+        'assets/images/content-slides/lunch-table-5929.jpg',
+        'assets/images/content-slides/photo-of-vegetable-salad-in-bowls-1640770.jpg',
+        'assets/images/content-slides/variety-of-vegetables-1400172.jpg',
+        'assets/images/content-slides/vegetables-and-tomatoes-on-cutting-board-255501.jpg'
+    ]
+    let landingCounter = 0
+    let contentCounter = 0
 
     let changeSearchBackgroundImage = ()=>{
         $('.landing').css({
-            backgroundImage : 'url("'+images[counter]+'")'
+            backgroundImage : 'url("'+landingImages[landingCounter]+'")'
         })
 
-        if(counter >= 5){
-            counter = 0
+        if(landingCounter >= 5){
+            landingCounter = 0
         }else{
-            counter ++
+            landingCounter ++
+        }
+        
+    }
+
+
+    let changeContentBackgroundImage = ()=>{
+        $('.content').css({
+            backgroundImage : 'url("'+contentImages[contentCounter]+'")'
+        })
+
+        if(contentCounter >= 6){
+            contentCounter = 0
+        }else{
+            contentCounter ++
         }
     }
 
-    setInterval(changeSearchBackgroundImage,10000)
+    setInterval(changeSearchBackgroundImage,20000)
+    setInterval(changeContentBackgroundImage,30000)
 
     $(window).scroll(()=>{
         if(window.pageYOffset >20){
